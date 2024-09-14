3cc463716c2399603c6c652362f3c06d6665c34b
Michael <84879204+M-Trevizo@users.noreply.github.com>
2024-09-13 18:39:09 -0700

    Add click listeners to buttons

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //TODO: Complete event handlers
 
+const submitButton = document.getElementById('');
+const clearButton = document.getElementById('');
+
 const handleSubmit = event => {
     event.preventDefault();
     // Submit the form to the API
@@ -9,3 +12,6 @@ const handleClear = event => {
     event.preventDefault();
     // Clear the form
 }
+
+submitButton.addEventListener('click', handleSubmit);
+clearButton.addEventListener('click', handleClear);
