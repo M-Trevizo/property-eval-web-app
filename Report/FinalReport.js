96bffbdde2a747427408b2c479a97b9d46aeeaf6
Michael <84879204+M-Trevizo@users.noreply.github.com>
2024-09-27 14:08:45 -0700

    Initial commit of Report page files

---
+++ Report/FinalReport.js
@@ -0,0 +1,11 @@
+export default class FinalReport {
+    estimate;
+
+    constructor(estimate) {
+        this.estimate = estimate;
+    }
+
+    getEstimate() {
+        return this.estimate;
+    }
+}
---
+++ Report/report.js
@@ -0,0 +1,4 @@
+//import { formData } from "../Index";
+
+const estimateText = document.getElementById("estimate");
+estimateText.innerText = `\$${600000}`;
