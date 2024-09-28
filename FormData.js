229b6a6db839c3ac5ab3a6045bf25eeccd564e65
Michael <84879204+M-Trevizo@users.noreply.github.com>
2024-09-28 11:49:58 -0700

    Add setters

--- FormData.js
+++ FormData.js
@@ -17,19 +17,39 @@ export default class FormData {
         return this.address;
     }
 
+    setAddress(address) {
+        this.address = address;
+    }
+
     getPropertySqft() {
         return this.propertySqft;
+    }
+
+    setPropertySqft(propertySqft) {
+        this.propertySqft = propertySqft;
     }
 
     getNumBedrooms() {
         return this.numBedrooms;
     }
 
+    setNumBedrooms(numBedrooms) {
+        this.numBedrooms = numBedrooms;
+    }
+
     getNumBathrooms() {
         return this.numBathrooms;
     }
 
+    setNumBathrooms(numBathrooms) {
+        this.numBathrooms = numBathrooms;
+    }
+
     getYearBuilt() {
         return this.yearBuilt;
+    }
+
+    setYearBuilt(yearBuilt) {
+        this.yearBuilt = yearBuilt;
     }
 }
