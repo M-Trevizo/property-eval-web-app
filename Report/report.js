ccac0e3489b25dcaabe771b7ff5675f0491c5e8e
Michael <84879204+M-Trevizo@users.noreply.github.com>
2024-09-28 02:33:53 -0700

    Get the report files working

--- Report/report.js
+++ Report/report.js
@@ -1,4 +1,22 @@
-//import { formData } from "../Index";
+import API from '../API/api.js';
+import FormData from '../FormData.js'
 
+const apiBaseUrl = `http://localhost:3000`;
+const api = new API(apiBaseUrl);
+
+const address = document.getElementById("address");
 const estimateText = document.getElementById("estimate");
-estimateText.innerText = `\$${600000}`;
+
+const data = await api.getFormData();
+const formData = new FormData(
+    data.address,
+    data.propertySqft,
+    data.numBedrooms,
+    data.numBathrooms,
+    data.yearBuilt
+);
+
+const estimate = await api.evaluateProperty();
+
+address.innerText = `${formData.address}`;
+estimateText.innerText = `\$${estimate}`;
